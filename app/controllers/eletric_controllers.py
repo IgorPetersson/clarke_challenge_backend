dd0d7b53aaeea8e44a6dff32c6f0e9f896d2f457
igor <igorpeterssontrab@gmail.com>
2022-01-29 22:45:47 -0400

    created the function to search for user suppliers by the minimum kwh limit

---
+++ app/controllers/eletric_controllers.py
@@ -0,0 +1,20 @@
+from app.models.eletric_models import Electric
+from http import HTTPStatus
+from flask_jwt_extended import jwt_required
+
+@jwt_required()
+def get_provider_for_kwh(kwh):
+    providers_electric = Electric.query.filter(Electric.mininum_limit_khw < kwh).all()
+    providers = [{
+        "id": provider.id,
+        "name": provider.name,
+        "logo": provider.logo,
+        "state": provider.state,
+        "minimum_limit_khw": provider.mininum_limit_kwh,
+        "average_rating": provider.average_rating,
+        "number_of_clients": provider.number_of_clients,
+        "price_kwh": provider.price_kwh
+    } for provider in providers_electric]
+
+    return {"data": providers}, HTTPStatus.OK
+   
