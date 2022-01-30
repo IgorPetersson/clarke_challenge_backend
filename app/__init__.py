cb7e5e006ac77fed586c2906c92b231544dcf7b9
igor <igorpeterssontrab@gmail.com>
2022-01-29 23:40:10 -0400

    feat: added lib flask_cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from sre_parse import FLAGS
 from flask import Flask
-from app.configs import env_configs, jwt_auth, database, migrations
+from app.configs import env_configs, jwt_auth, database, migrations, cors
 from app import routes
 
 def create_app():
     app = Flask(__name__)
+    cors.init_app(app)
     database.init_app(app)
     migrations.init_app(app)
     env_configs.init_app(app)
